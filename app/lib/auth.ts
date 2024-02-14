25a9224cdbc8629f06e9ec18697cc4f46749e419
Kristian <kristianhnielsen@protonmail.com>
2024-02-14 22:45:56 +0800

    add login page and boilerplate for signup, login, and signout

---
+++ app/lib/auth.ts
@@ -0,0 +1,61 @@
+"use server";
+
+import { headers, cookies } from "next/headers";
+import { getSupabaseClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
+
+export const signIn = async (formData: FormData) => {
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = getSupabaseClient();
+
+  const { error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  return redirect("/account");
+};
+
+export const signUp = async (formData: FormData) => {
+  const origin = headers().get("origin");
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const supabase = getSupabaseClient();
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  });
+
+  if (error) {
+    return redirect("/login?message=Could not authenticate user");
+  }
+
+  return redirect("/login?message=Check email to continue sign in process");
+};
+
+export const signOut = async () => {
+  const supabase = getSupabaseClient();
+
+  await supabase.auth.signOut();
+  return redirect("/");
+};
+
+export const getUser = async () => {
+  const supabase = getSupabaseClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  return user;
+};
---
+++ app/login/page.tsx
@@ -0,0 +1,59 @@
+import { getUser, signIn, signUp } from "../lib/auth";
+import { redirect } from "next/navigation";
+
+export default async function Login({
+  searchParams,
+}: {
+  searchParams: { message: string };
+}) {
+  const user = await getUser();
+  if (user) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <h1 className="text-primary text-2xl font-bold">Login</h1>
+      <div className="flex-1 flex flex-col px-8 sm:max-w-md justify-center gap-2 w-full h-screen">
+        <form
+          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-light"
+          action={signIn}
+        >
+          <label className="text-md" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            name="email"
+            placeholder="you@example.com"
+            required
+          />
+          <label className="text-md" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            type="password"
+            name="password"
+            placeholder="••••••••"
+            required
+          />
+          <button className="bg-green-700 rounded-md px-4 py-2 text-light mb-2">
+            Sign In
+          </button>
+          <button
+            formAction={signUp}
+            className="border border-light/20 rounded-md px-4 py-2 text-light mb-2"
+          >
+            Sign Up
+          </button>
+          {searchParams?.message && (
+            <p className="mt-4 p-4 bg-light/10 text-light text-center">
+              {searchParams.message}
+            </p>
+          )}
+        </form>
+      </div>
+    </>
+  );
+}
