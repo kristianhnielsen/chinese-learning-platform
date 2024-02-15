ce3305f88e207723f68d80468c72874ca2301eaf
Kristian <kristianhnielsen@protonmail.com>
2024-02-15 22:31:10 +0800

    formatting

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,47 +8,43 @@ export default async function Login({
 }) {
   const user = await getAuthUser();
   if (user) {
-    redirect("/");
+    redirect("/account");
   }
 
   return (
     <>
-      <h1 className="text-primary text-2xl font-bold">Login</h1>
-      <div className="flex-1 flex flex-col px-8 sm:max-w-md justify-center gap-2 w-full h-screen">
+      <h1 className="text-2xl font-bold text-primary">Login</h1>
+      <div className="flex h-screen w-full flex-1 flex-col justify-center gap-2 px-8 sm:max-w-md">
         <form
-          className="animate-in flex-1 flex flex-col w-full justify-center gap-2 text-light"
+          className="animate-in flex w-full flex-1 flex-col justify-center gap-2 text-light"
           action={signIn}
         >
-          <label className="text-md" htmlFor="email">
-            Email
-          </label>
+          <label htmlFor="email">Email</label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="mb-6 rounded-md border bg-inherit px-4 py-2"
             name="email"
             placeholder="you@example.com"
             required
           />
-          <label className="text-md" htmlFor="password">
-            Password
-          </label>
+          <label htmlFor="password">Password</label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="mb-6 rounded-md border bg-inherit px-4 py-2"
             type="password"
             name="password"
             placeholder="••••••••"
             required
           />
-          <button className="bg-green-700 rounded-md px-4 py-2 text-light mb-2">
+          <button className="mb-2 rounded-md bg-green-700 px-4 py-2 text-light">
             Sign In
           </button>
           <button
             formAction={signUp}
-            className="border border-light/20 rounded-md px-4 py-2 text-light mb-2"
+            className="mb-2 rounded-md border border-light/20 px-4 py-2 text-light"
           >
             Sign Up
           </button>
           {searchParams?.message && (
-            <p className="mt-4 p-4 bg-light/10 text-light text-center">
+            <p className="mt-4 rounded-md bg-light/10 p-4 text-center text-light">
               {searchParams.message}
             </p>
           )}
