3fb39eeadbf3129d797f51a7dad5aa153a6aeec1
Kristian <kristianhnielsen@protonmail.com>
2024-03-25 21:38:02 +0800

    brand is not link + responsiveness

--- app/ui/SideNav.tsx
+++ app/ui/SideNav.tsx
@@ -13,44 +13,37 @@ import { getAuthUser, signOut } from "../lib/auth";
 export default async function SideNav() {
   const authUser = await getAuthUser();
   return (
-    <nav className="sticky top-0 flex max-h-dvh min-h-full flex-col items-center gap-4 border-r-4 border-light p-4 text-light transition-all">
-      <SideNavLink
-        href="/"
-        className="flex w-auto flex-col gap-0 p-1 leading-tight outline-none outline-2 outline-primary"
-      >
+    <nav className="sticky top-0 flex min-h-full flex-col items-center gap-4 border-r-4 border-light p-2 py-6 text-light transition-all md:p-4">
+      <p className="flex aspect-square w-auto flex-col items-center gap-0 rounded p-0 text-xs leading-tight outline-none outline-2 outline-primary md:w-12 md:text-base">
         <span>易字</span>
         <span>词典</span>
-      </SideNavLink>
+      </p>
       <SideNavLink href="/">
         <FaLanguage className="h-full w-full" />
       </SideNavLink>
       <SideNavLink href="/most-common">
         <FaArrowDown19 className="h-full w-full" />
       </SideNavLink>
-      <SideNavLink href="/hsk" className="font-bold">
-        HSK
-      </SideNavLink>
+      <SideNavLink href="/hsk">HSK</SideNavLink>
       <SideNavLink href="/learn">
         <FaBookOpenReader className="h-full w-full" />
       </SideNavLink>
-      <div className="mt-auto grid gap-4">
+      <div className="mt-auto grid justify-items-center gap-4">
         <SideNavLink href={"/account"}>
           <FaCircleUser
             className={twMerge(
-              "h-full w-full rounded-full border-2 p-0.5",
+              "h-full w-full rounded-full border-[1px] p-0.5 md:border-2",
               authUser ? "border-cyan-800" : "border-yellow-500",
             )}
           />
         </SideNavLink>
         {authUser && (
-          <form
-            action={signOut}
-            className="group flex aspect-square w-12 items-center gap-2 rounded p-2 transition-all hover:bg-light/10"
+          <button
+            className="group flex aspect-square w-10 items-center gap-2 rounded p-2 transition-all hover:bg-light/10 md:w-12"
+            formAction={signOut}
           >
-            <button className="w-full">
-              <FaRightFromBracket className="h-full w-full transition-all group-hover:fill-red-400" />
-            </button>
-          </form>
+            <FaRightFromBracket className="h-full w-full transition-all group-hover:fill-red-400" />
+          </button>
         )}
       </div>
     </nav>
@@ -69,7 +62,7 @@ function SideNavLink({
   return (
     <Link
       className={twMerge(
-        "flex aspect-square w-12 items-center gap-2 rounded p-2 transition-all hover:bg-light/10",
+        "flex aspect-square w-8 items-center gap-2 rounded p-1 text-center text-xs font-bold transition-all hover:bg-light/10 md:w-12 md:p-2 md:text-base",
         extClassName,
       )}
       href={href}
