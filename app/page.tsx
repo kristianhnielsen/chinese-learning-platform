42835900523d1f061b6f2048bff4a56e90e62457
Kristian <kristianhnielsen@protonmail.com>
2024-04-09 20:57:11 +0800

    redirect from signup to login page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import DictSearch from "./ui/DictSearch";
 import DictEntries from "./ui/DictEntries";
+import { redirect } from "next/navigation";
 
 export default function Home({
   searchParams,
 }: {
   searchParams?: {
     query?: string;
+    code?: string;
   };
 }) {
+  if (searchParams?.code) {
+    // redirected from signup confirmation email
+    redirect("/login");
+  }
   return (
     <>
       <hgroup className="grid items-center gap-2 text-center">
